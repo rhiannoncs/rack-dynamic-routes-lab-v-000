df4a77f0fbb45c36ec8df16807ac8c6e20cad2c1
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-02-28 19:35:15 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = 
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    
+    resp.finish
+  end
+end
